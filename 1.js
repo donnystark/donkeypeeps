aefe99763edcd41bd87c9c425aead1394df42afb
Donny <106444792+donnystark@users.noreply.github.com>
2023-05-26 03:26:10 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,21 @@
-function toggleLight() {
-    var light = document.querySelector('.light');
-    light.classList.toggle('on');
-  }
-  
+// Get the car element
+const car = document.querySelector('.car');
+
+// Set initial position and speed
+let position = 0;
+const speed = 5; // Adjust this value to control the car speed
+
+// Function to move the car
+function moveCar() {
+  // Update the position
+  position += speed;
+
+  // Apply the position to the car element
+  car.style.left = position + 'px';
+
+  // Request the next frame
+  requestAnimationFrame(moveCar);
+}
+
+// Start moving the car
+moveCar();
